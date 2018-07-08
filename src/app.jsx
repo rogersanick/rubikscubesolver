eb0ee0a1be61dd840ddeab80b45757f59e098e86
rogersanick <rogers.a.nick@gmail.com>
2018-07-07 21:40:24 -0400

    Committing after installing webpack

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (<div>
+      Hello world!
+    </div>)
+  }
+}
