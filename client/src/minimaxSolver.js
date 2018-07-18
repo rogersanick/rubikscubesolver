ce2470953ab0373f48931b7a8350f1b30d058bc8
rogersanick <rogers.a.nick@gmail.com>
2018-07-17 22:08:16 -0400

    Refactored solver function to detect whether cubes were in the right position rather than sides

--- client/src/minimaxSolver.js
+++ client/src/minimaxSolver.js
@@ -1,4 +1,7 @@
 import rubiks from './cube-functions.js';
+import cubeSideMapping from './cube-side-mapping.js';
+import solution from './solutionCube.js';
+
 
 const moves = ['F', 'Fi', 'B', 'Bi', 'L', 'Li', 'R', 'Ri', 'U', 'Ui', 'D', 'Di'];
 
@@ -104,8 +107,36 @@ self.addEventListener('message', function(e) {
   })
 });
 
-
 export const getScore = (rubiksArray) => {
+
+  let correctScore = 0;
+  let maxScore = 27;
+
+  let newStateMapping = JSON.parse(JSON.stringify(cubeSideMapping));
+
+  for (let cube in newStateMapping) {
+    for (let face in newStateMapping[cube]) {
+      if (newStateMapping[cube][face]) {
+        newStateMapping[cube][face] = rubiksArray[newStateMapping[cube][face][0]][newStateMapping[cube][face][1]];
+      };
+    }
+  }
+
+  for (let cube in solution) {
+    for (let face in solution[cube]) {
+      if (solution[cube][face]) {
+        if (solution[cube][face] === rubkis[cube][face]) {
+          correctScore++;
+        };
+      }
+    }
+  }
+
+  return correctScore / maxScore * 100;
+
+}
+
+export const getScoreOld = (rubiksArray) => {
   let correctScore = 0;
   let maxScore = 54;
   let solution = [
@@ -116,6 +147,7 @@ export const getScore = (rubiksArray) => {
     Array(9).fill('Y'),
     Array(9).fill('G'),
   ];
+
   for (let face = 0; face < 6; face++) {
     for (let piece = 0; piece < 9; piece++) {
       if (solution[face][piece] === rubiksArray[face][piece]) {
